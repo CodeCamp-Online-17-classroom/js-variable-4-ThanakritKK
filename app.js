89250f430a879047d60858c8219483d7914a1a58
ThanakritKK <xseigh.xc@gmail.com>
2024-06-24 20:31:26 +0700

    My name

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // write code here
+
+const FirstName = "Thanakrit";
+let lastName = "Kaewsringam";
+var nickname = "Jay";
+
+console.log(FirstName + lastName + nickname);
+
+const FirstName = "Sawada";
+let lastName = "Tsunayoshi";
+var nickname = "Tsuna";
+
+birthDate = "1/1/2001";
+
